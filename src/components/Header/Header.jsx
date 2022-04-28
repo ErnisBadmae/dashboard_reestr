bd375fc335d5545e4ec44fbc98bff8318045649b
Ernis <e.badmaev@gmail.com>
2022-04-28 17:09:29 +0300

    headerCss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,15 @@
-import logo from '../../assets/img/logo/Logo.svg'
-import descLogo from '../../assets/img/desc/desc.svg'
-import './header.scss'
+import logo from '../../assets/img/logo/Logo.svg';
+import descLogo from '../../assets/img/desc/desc.svg';
+import {
+  SearchOutlined,
+  NotificationsNoneOutlined,
+  ChatBubbleOutlineOutlined,
+  LanguageOutlined,
+  Settings,
+  AccountCircle,
+  PowerSettingsNew,
+} from '@mui/icons-material';
+import './header.scss';
 
 function Header(props) {
   return (
@@ -8,17 +17,33 @@ function Header(props) {
       <header className="header">
         <div className="header__inner">
           <div className="header__logo">
-            <img className='header__logo-1' src={logo} alt='logo'/>
-            <img className='header__logo-2' src={descLogo} alt='logo'/>
+            <img className="header__logo-1" src={logo} alt="logo" />
+
+            <img className="header__logo-2" src={descLogo} alt="logo" />
           </div>
           <div className="header__icons">
-            <input className='header__input' />
-            <img className='header__icon-x' src={logo} alt='logo'/>
-            <img className='header__icon-x' src={logo} alt='logo'/>
-            <img className='header__icon-x' src={logo} alt='logo'/>
-            <img className='header__icon-x' src={logo} alt='logo'/>
-            <img className='header__icon-x' src={logo} alt='logo'/>
-            <img className='header__icon-x' src={logo} alt='logo'/>
+            <div className="header__icons-search">
+              <input className="header__input" />
+              <SearchOutlined className="icon"/>
+            </div>
+            <div className="item">
+              <NotificationsNoneOutlined className="icon" />
+            </div>
+            <div className="item">
+              <ChatBubbleOutlineOutlined className="icon" />
+            </div>
+            <div className="item">
+              <LanguageOutlined className="icon" />
+            </div>
+            <div className="item">
+              <Settings className="icon" />
+            </div>
+            <div className="item">
+              <AccountCircle className="icon" />
+            </div>
+            <div className="item">
+              <PowerSettingsNew className="icon" />
+            </div>
           </div>
         </div>
       </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import Header from '../components/Header/Header';
 import Navbar from '../components/Navbar/Navbar';
-import Sidebar from '../components/Sidebar/Sidebar';
 import Main from '../components/Main/Main';
 
 function Home(props) {
