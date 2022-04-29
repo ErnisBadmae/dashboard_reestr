4a98cc8f6a6fa5de0b336528799c4662d9b31634
Ernis <e.badmaev@gmail.com>
2022-04-29 12:31:56 +0300

    sidebar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import logo from '../../assets/img/logo/Logo.svg';
 import logo2 from '../../assets/img/logo/logo2.png';
+import icon from '../../assets/img/icons/icon1.png';
+
 import './footer.scss';
 
 function Footer(props) {
@@ -7,35 +9,55 @@ function Footer(props) {
     <footer className="footer">
       <div className="footer__inner">
         <div className="footer__logo">
-          <img className="footer__logo-1" src={logo} alt="logo" />
-          <img className="footer__logo-2" src={logo2} alt="logo2" />
-          <span className='footer__logo-text'>ТК 066</span>
+          <div className="footer__logo-block">
+            <img className="footer__logo-1" src={logo} alt="logo" />
+            <img className="footer__logo-2" src={logo2} alt="logo2" />
+          </div>
+          <div className="footer__logo-tk">
+            <p className="footer__logo-text">ТК 066</p>
+            <p className="footer__logo-lk">Личный кабинет</p>
+          </div>
         </div>
-        
+
         <div className="footer__content">
-          <div className="footer__content-1">
-            <span >Заявление СДС</span>
-            <span>Заявление ОС</span>
-            <span>Сертификация</span>
-            <span>Аттестация Эксперта</span>
+          <div className="footer__content-items">
+            <p className="footer__content-item">Заявление СДС</p>
+            <p className="footer__content-item">Заявление ОС</p>
+            <p className="footer__content-item">Сертификация</p>
+            <p className="footer__content-item">Аттестация Эксперта</p>
           </div>
-          <div className="footer__content-2">
-            <span>Реестр СДС</span>
-            <span>Реестр ОС</span>
-            <span>Реестр Экспертов</span>
-            <span>Реестр Сертификатов</span>
+          <div className="footer__content-items">
+            <p className="footer__content-item">Реестр СДС</p>
+            <p className="footer__content-item">Реестр ОС</p>
+            <p className="footer__content-item">Реестр Экспертов</p>
+            <p className="footer__content-item">Реестр Сертификатов</p>
           </div>
-          <div className="footer__content-3">
-            <div className="footer__content-1">
-              <span>Сообщения</span>
-              <span>Пользователи</span>
-              <span>Настройки</span>
-              <span>Отчеты</span>
-            </div>
+          <div className="footer__content-items">
+            <p className="footer__content-item">Сообщения</p>
+            <p className="footer__content-item">Пользователи</p>
+            <p className="footer__content-item">Настройки</p>
+            <p className="footer__content-item">Отчеты</p>
           </div>
         </div>
         <div className="footer__contact">
-          <span>Москва Нахимовский проспект</span>
+          <div className="footer__address">
+            <a className="footer__address-click" href="#">
+              <div className="footer__address-p">
+                <p>Москва </p>
+                <p>Нахимовский проспект</p>
+                <p>дом 31 корп 2</p>
+              </div>
+            </a>
+            <img className="footer__icon-1" src={icon} alt="icon" />
+          </div>
+          <div className="footer__rasp">
+            <p className='footer__rasp-text'>расписанеи</p>
+            <img className="footer__icon-1" src={icon} alt="icon" />
+          </div>
+          {/* <p>Москва Нахимовский проспект</p> */}
+          {/* <p>Москва Нахимовский проспек</p>
+          <p>Москва Нахимовский</p>
+          <p>Москва Нахим</p> */}
         </div>
       </div>
     </footer>
