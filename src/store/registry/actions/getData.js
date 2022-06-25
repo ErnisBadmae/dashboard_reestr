1836e95fa234b605d57f54590fd47e37bf7382de
Ernis <e.badmaev@gmail.com>
2022-06-26 00:53:54 +0300

    fix: warning from flexible

--- src/store/registry/actions/getData.js
+++ src/store/registry/actions/getData.js
@@ -11,8 +11,17 @@ export const getData = createAsyncThunk('dataRegistry/get', async (payload) => {
         return {
             ...el,
             certificate_date: correctlyDate(el.certificate_date),
-            valid_date: correctlyDate(el.valid_date),
+            valid: correctlyDate(el.valid),
             registration_date: correctlyDate(el.registration_date),
+            statusId: el.status?.id,
+            statusTitle: el.status
+                ? el.status?.title
+                : el.status_certificate
+                ? el.status_certificate?.title
+                : '',
+            osValue:
+                el.organ_certification_certificate
+                    ?.short_name_organ_certification,
         };
     });
     return value;
